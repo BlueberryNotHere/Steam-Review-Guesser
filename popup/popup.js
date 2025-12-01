268dad3a20f34d3004292384378d21cd868f5c69
BlueberryNotHere <mrblueberrynothere@gmail.com>
2025-11-30 23:04:53 -0500

    Update popup.js

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,16 @@
+//ELEMENTS
+const includeTagsElement = document.getElementById("includeTags")
+const excludeTagsElement = document.getElementById("excludeTags")
+const startDateElement = document.getElementById("startDate")
+const endDateElement = document.getElementById("endDate")
 
+//BUTTONS
+const saveButton = document.getElementById("saveButton")
+
+//CHECKBOXES
+const includeAllButton = document.getElementById("includeAll")
+
+saveButton.onClick = function()
+{
+  console.log("Save Button")
+}
