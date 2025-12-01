61cbed300e04ed2a7c455604dc31f8334e0032c9
BlueberryNotHere <mrblueberrynothere@gmail.com>
2025-11-30 21:50:19 -0500

    Update nextGame.js

--- src/nextGame.js
+++ src/nextGame.js
@@ -1,4 +1,4 @@
-const sleep = ms => new Promise(res => setTimeout(res, ms))
+//const sleep = ms => new Promise(res => setTimeout(res, ms))
   
 (function (root) {
   const ns = (root.ReviewGuesser = root.ReviewGuesser || {});
