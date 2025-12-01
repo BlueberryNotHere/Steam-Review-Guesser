fef2b9f361eb9c8ab4973a7b4a119a074d158cce
BlueberryNotHere <mrblueberrynothere@gmail.com>
2025-11-30 23:37:17 -0500

    Update background.js

--- popup/background.js
+++ popup/background.js
@@ -1,8 +1,14 @@
 chrome.runtime.onMessage.addListener(data => {
-  switch (data.event) {
+  const {event, prefs} = data
+  switch (event) {
     case 'saveData':
+      handleSaveData(prefs);
       break;
     default:
       break;
   }
 })
+
+const handleSaveData = (prefs) => {
+  chrome.storage.local.set(prefs)
+}
